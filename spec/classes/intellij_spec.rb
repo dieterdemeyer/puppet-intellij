e3167255d03bc8998ca681fd1398a0453eb34855
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-09 11:25:51 +0100

    Added spec tests

---
+++ spec/classes/intellij_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'intellij' do
+
+  context 'with faulty input' do 
+    context 'with edition => foo' do
+      let (:params) { { :edition=> 'foo' } }
+      
+      it { expect { subject }.to raise_error(
+        Puppet::Error, /parameter edition must be community or ultimate/
+      )}
+    end
+  end
+
+  context 'with default parameters' do
+    let (:params) { { } }
+
+    it { should include_class('intellij') }
+    
+    it { should contain_package('IntelliJ-IDEA-IC-12.0.4').with_provider('appdmg') }
+    it { should contain_package('IntelliJ-IDEA-IC-12.0.4').with_source('http://download.jetbrains.com/idea/ideaIC-12.0.4.dmg') }
+  end
+
+  context 'with edition => ultimate' do
+    let (:params) { { :edition => 'ultimate' } }
+
+    it { should include_class('intellij') }
+
+    it { should contain_package('IntelliJ-IDEA-IU-12.0.4').with_provider('appdmg') }
+    it { should contain_package('IntelliJ-IDEA-IU-12.0.4').with_source('http://download.jetbrains.com/idea/ideaIU-12.0.4.dmg') }
+  end
+
+end
