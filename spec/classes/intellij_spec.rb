ed92e9b3c3e812a2fa8bf7a6b4b498ceb5bf79d2
Csaba Palfi <csaba@palfi.me>
2013-05-06 08:40:08 +0100

    update version to 12.1.2

--- spec/classes/intellij_spec.rb
+++ spec/classes/intellij_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'intellij' do
 
-  context 'with faulty input' do 
+  context 'with faulty input' do
     context 'with edition => foo' do
       let (:params) { { :edition=> 'foo' } }
-      
+
       it { expect { subject }.to raise_error(
         Puppet::Error, /parameter edition must be community or ultimate/
       )}
@@ -16,9 +16,9 @@ describe 'intellij' do
     let (:params) { { } }
 
     it { should contain_class('intellij') }
-    
-    it { should contain_package('IntelliJ-IDEA-IC-12.1').with_provider('appdmg') }
-    it { should contain_package('IntelliJ-IDEA-IC-12.1').with_source('http://download.jetbrains.com/idea/ideaIC-12.1.dmg') }
+
+    it { should contain_package('IntelliJ-IDEA-IC-12.1.2').with_provider('appdmg') }
+    it { should contain_package('IntelliJ-IDEA-IC-12.1.2').with_source('http://download.jetbrains.com/idea/ideaIC-12.1.2.dmg') }
   end
 
   context 'with edition => ultimate' do
@@ -26,8 +26,8 @@ describe 'intellij' do
 
     it { should contain_class('intellij') }
 
-    it { should contain_package('IntelliJ-IDEA-IU-12.1').with_provider('appdmg') }
-    it { should contain_package('IntelliJ-IDEA-IU-12.1').with_source('http://download.jetbrains.com/idea/ideaIU-12.1.dmg') }
+    it { should contain_package('IntelliJ-IDEA-IU-12.1.2').with_provider('appdmg') }
+    it { should contain_package('IntelliJ-IDEA-IU-12.1.2').with_source('http://download.jetbrains.com/idea/ideaIU-12.1.2.dmg') }
   end
 
 end
